cad8a203bbb7a049dff13326500fdfb3e39c6e56
dangj501 <dangja9@gmail.com>
2024-03-07 15:32:46 -0600

    ajax.html

--- practica00/src/main/java/mx/uv/App.java
+++ practica00/src/main/java/mx/uv/App.java
@@ -16,5 +16,26 @@ public class App
             response.type("application/xml");
             return respuesta;
         });
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        System.out.println("Hello World!");
+
+        get("/xml", (request, response) -> {
+            String respuesta = "<x><y>algo<y/></x>";
+            response.type("application/xml");
+            return respuesta;
+        });
     }
 }
