c459e4b1351a24b82752a1598bff91e79dbb84a0
dangj501 <dangja9@gmail.com>
2024-04-04 15:28:11 -0600

    Practica03

--- Practica02/Services/Operaciones.cs
+++ Practica02/Services/Operaciones.cs
@@ -1,15 +1,29 @@
 using System;
 using WSDL.mensajes;
+using System.Collections;
 
 namespace WSDL.operaciones {
 	public class Operaciones : Mensajes {
+		ArrayList Lista = new ArrayList();
 		public string Saludar (string nombre){
 			string msj = "hola " + nombre;
 			return msj;
 		}
 
-		public string Mostrar (int id){
-			return "x";
-		}
+		public string Mostrar(int id)
+{
+    // Verificar si el índice está dentro del rango
+    if (id >= 0 && id < Lista.Count)
+    {
+        // Devolver el elemento correspondiente al id
+        return Lista[id].ToString();
+    }
+    else
+    {
+        // Si el índice está fuera del rango, devolver un mensaje de error
+        return "ID fuera de rango";
+    }
+}
+
 	}
 }
